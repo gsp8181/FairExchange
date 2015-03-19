1c407855d264774bf58ea7aba80e9b57b8dbb39c
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-03-19 18:33:35 +0000

    Added short config checking script

---
+++ WebService/src/main/java/uk/co/gsp8181/ttp/configcheck/ConfigCheckRESTService.java
@@ -0,0 +1,40 @@
+package uk.co.gsp8181.ttp.configcheck;
+
+import javax.ejb.Stateless;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.net.Socket;
+
+@Path("/config")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Stateless
+public class ConfigCheckRESTService {
+
+    @GET
+    @Path("/ip")
+    public String returnIp(@Context HttpServletRequest req)
+    {
+        String remoteAddr = req.getRemoteAddr();
+        return remoteAddr;
+    }
+
+    @GET
+    @Path("/port")
+    public boolean isPortOpen(@Context HttpServletRequest req)
+    {
+        String remoteAddr = req.getRemoteAddr();
+        try (Socket ignored = new Socket(remoteAddr, 80)) {
+            return true;
+        } catch (IOException ignored) {
+            return false;
+        }
+    }
+
+}
