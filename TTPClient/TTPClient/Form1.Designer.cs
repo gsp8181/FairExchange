f84f9f81f5d94bfe107280f24fcbb9aa3884e374
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-03-20 15:39:47 +0000

    Added the worlds worst client

---
+++ TTPClient/TTPClient/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TTPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.whatsMyIpButton = new System.Windows.Forms.Button();
+            this.regWithTrackerButton = new System.Windows.Forms.Button();
+            this.PortOpenButton = new System.Windows.Forms.Button();
+            this.getRemoteStatus = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.emailBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // whatsMyIpButton
+            // 
+            this.whatsMyIpButton.Location = new System.Drawing.Point(12, 85);
+            this.whatsMyIpButton.Name = "whatsMyIpButton";
+            this.whatsMyIpButton.Size = new System.Drawing.Size(106, 23);
+            this.whatsMyIpButton.TabIndex = 0;
+            this.whatsMyIpButton.Text = "Whats My IP";
+            this.whatsMyIpButton.UseVisualStyleBackColor = true;
+            this.whatsMyIpButton.Click += new System.EventHandler(this.whatMyIp_Click);
+            // 
+            // regWithTrackerButton
+            // 
+            this.regWithTrackerButton.Location = new System.Drawing.Point(12, 114);
+            this.regWithTrackerButton.Name = "regWithTrackerButton";
+            this.regWithTrackerButton.Size = new System.Drawing.Size(106, 23);
+            this.regWithTrackerButton.TabIndex = 1;
+            this.regWithTrackerButton.Text = "Reg With Tracker";
+            this.regWithTrackerButton.UseVisualStyleBackColor = true;
+            this.regWithTrackerButton.Click += new System.EventHandler(this.regWithTrackerButton_Click);
+            // 
+            // PortOpenButton
+            // 
+            this.PortOpenButton.Location = new System.Drawing.Point(12, 143);
+            this.PortOpenButton.Name = "PortOpenButton";
+            this.PortOpenButton.Size = new System.Drawing.Size(106, 23);
+            this.PortOpenButton.TabIndex = 2;
+            this.PortOpenButton.Text = "6555 Open?";
+            this.PortOpenButton.UseVisualStyleBackColor = true;
+            this.PortOpenButton.Click += new System.EventHandler(this.PortOpenButton_Click);
+            // 
+            // getRemoteStatus
+            // 
+            this.getRemoteStatus.Location = new System.Drawing.Point(12, 173);
+            this.getRemoteStatus.Name = "getRemoteStatus";
+            this.getRemoteStatus.Size = new System.Drawing.Size(106, 23);
+            this.getRemoteStatus.TabIndex = 3;
+            this.getRemoteStatus.Text = "Get IP";
+            this.getRemoteStatus.UseVisualStyleBackColor = true;
+            this.getRemoteStatus.Click += new System.EventHandler(this.getRemoteStatus_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "http://localhost:8080";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TTP Server: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Email:";
+            // 
+            // emailBox
+            // 
+            this.emailBox.Location = new System.Drawing.Point(86, 38);
+            this.emailBox.Name = "emailBox";
+            this.emailBox.Size = new System.Drawing.Size(186, 20);
+            this.emailBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.emailBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.getRemoteStatus);
+            this.Controls.Add(this.PortOpenButton);
+            this.Controls.Add(this.regWithTrackerButton);
+            this.Controls.Add(this.whatsMyIpButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button whatsMyIpButton;
+        private System.Windows.Forms.Button regWithTrackerButton;
+        private System.Windows.Forms.Button PortOpenButton;
+        private System.Windows.Forms.Button getRemoteStatus;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox emailBox;
+    }
+}
+
---
+++ TTPClient/TTPClient/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTPClient
+{
+    public partial class Form1 : Form
+    {
+        WebClient syncClient = new WebClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void whatMyIp_Click(object sender, EventArgs e)
+        {
+            var url = textBox1.Text + "/rest/config/ip/";
+            
+            var content = syncClient.DownloadString(url);
+            MessageBox.Show(content);
+        }
+
+        private void regWithTrackerButton_Click(object sender, EventArgs e)
+        {
+            byte[] emailBytes = System.Text.Encoding.UTF8.GetBytes(emailBox.Text);
+            var url = textBox1.Text + "/rest/sessions/";
+            var request = (HttpWebRequest)WebRequest.CreateHttp(url);
+            request.Method = "POST";
+            request.ContentLength = emailBytes.Length;
+            request.ContentType = "application/json";
+            var dataStream = request.GetRequestStream();
+            dataStream.Write(emailBytes, 0, emailBytes.Length);
+            dataStream.Close();
+            var response = (HttpWebResponse)request.GetResponse();
+            MessageBox.Show(response.StatusDescription);
+        }
+
+        private void getRemoteStatus_Click(object sender, EventArgs e)
+        {
+            var url = textBox1.Text + "/rest/sessions/";
+            string input = Microsoft.VisualBasic.Interaction.InputBox("Prompt", "Title", "Default", -1, -1);
+
+            url = url + input;
+            try
+            { 
+            var content = syncClient.DownloadString(url);
+            MessageBox.Show(content);
+                } catch (WebException ex)
+            {
+                MessageBox.Show("Could not find. " + ex.Message);
+            }
+            
+        }
+    }
+}
