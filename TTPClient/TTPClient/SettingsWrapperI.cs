2836696c7d5cc5b275716d654a008fbf498f7147
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-03-31 15:55:29 +0100

    Added SettingsWrapper

---
+++ TTPClient/TTPClient/SettingsWrapperI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTPClient
+{
+    interface SettingsWrapperI
+    {
+        void setEmail();
+        void setTTP();
+        void loadTTP();
+        void loadEmail();
+        bool isSet();
+        bool regWithTracker(string email, string tracker );
+        bool regWithTracker();
+    }
+}
