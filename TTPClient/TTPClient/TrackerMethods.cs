93b8ce768fbe0d8ab3a401b3029c477ecac6e564
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-04-02 15:56:29 +0100

    Added debug to project added tracker methods interface

---
+++ TTPClient/TTPClient/TrackerMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTPClient
+{
+    interface ITrackerMethods
+    {
+        string getIp(string email);
+        string getEmail(string ip);
+        string getPublicKey(string email);
+        void sendPem();
+
+    }
+}
