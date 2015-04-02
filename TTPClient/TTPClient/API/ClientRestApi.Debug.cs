f30ffd44a02e9df20407f93bc0b08651a81fb925
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-04-02 15:40:07 +0100

    Moved debug rest events to separate file

---
+++ TTPClient/TTPClient/API/ClientRestApi.Debug.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Grapevine;
+using Grapevine.Server;
+using Newtonsoft.Json.Linq;
+using TTPClient.Security;
+
+namespace TTPClient
+{
+    public sealed partial class ClientRestApi
+    {
+#if DEBUG
+        [RESTRoute(Method = HttpMethod.POST, PathInfo = @"^/decrypt/?$")]
+        public void HandleDecryptRequest(HttpListenerContext context)
+        {
+            var payload = JObject.Parse(this.GetPayload(context.Request));
+            var data = payload.Value<string>("data");
+            var key = payload.Value<string>("key");
+            context.Response.ContentType = "text/plain";
+            this.SendTextResponse(context, Rsa.DecryptData(data, key));
+        }
+
+        [RESTRoute(Method = HttpMethod.POST, PathInfo = @"^/encrypt/?$")]
+        public void HandleEncryptRequest(HttpListenerContext context)
+        {
+            //JObject response = new JObject();
+            //response.Add("Key",key);
+            //response.Add("data", data);
+            this.SendJsonResponse(context, Rsa.EncryptData(this.GetPayload(context.Request)));
+        }
+
+        [RESTRoute(Method = HttpMethod.POST, PathInfo = @"^/sign/?$")]
+        public void HandleSignRequest(HttpListenerContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            this.SendTextResponse(context, Rsa.SignData(this.GetPayload(context.Request)));
+        }
+        [RESTRoute(Method = HttpMethod.POST, PathInfo = @"^/encryptTo/?$")]
+        public void HandleEncryptToRequest(HttpListenerContext context)
+        {
+            var payload = JObject.Parse(this.GetPayload(context.Request));
+            var data = payload.Value<string>("data");
+            var key = payload.Value<string>("key");
+            this.SendJsonResponse(context, Rsa.EncryptData(data, key));
+        }
+#endif
+    }
+}
