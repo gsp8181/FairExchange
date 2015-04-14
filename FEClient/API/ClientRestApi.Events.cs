ed0867486a59fe5d5f5dc7b6ef5c765b958d8e4e
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-04-14 16:13:53 +0100

    Changed event handler so fxcop likes me again

--- FEClient/API/ClientRestApi.Events.cs
+++ FEClient/API/ClientRestApi.Events.cs
@@ -1,28 +1,16 @@
-﻿namespace FEClient.API
+﻿using System;
+
+namespace FEClient.API
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public sealed partial class ClientRestApi
     {
-        public delegate void FileRecievedAndRespSendHandler(object sender, FileSendEventArgs e);
-
-        public delegate void FileRecievedHandler(object sender, FileSendEventArgs e);
-
-        public delegate void FinishHandler(object sender, FinishEventArgs e);
-
-        public delegate void KeyRecievedHandler(object sender, KeyReceivedEventArgs e);
-
-        public delegate void NotifyRecievedHandler(object sender, NotifyRequestEventArgs e);
-
-        public delegate void StartTransmissionAndRespSentHandler(object sender, StartTransmissionEventArgs e);
-
-        public delegate void StartTransmissionHandler(object sender, StartTransmissionEventArgs e);
-
-        public static event FileRecievedHandler FileRecieved = delegate { };
-        public static event KeyRecievedHandler KeyRecieved = delegate { };
-        public static event FileRecievedAndRespSendHandler FileRecievedAndRespSent = delegate { };
-        public static event NotifyRecievedHandler NotifyRecieved = delegate { };
-        public static event StartTransmissionHandler StartTransmission = delegate { };
-        public static event FinishHandler Finish = delegate { };
-        public static event StartTransmissionAndRespSentHandler StartTransmissionAndRespSent = delegate { };
+        public static event EventHandler<FileSendEventArgs> FileRecieved;
+        public static event EventHandler<KeyReceivedEventArgs> KeyRecieved;
+        public static event EventHandler<FileSendEventArgs> FileRecievedAndRespSent;
+        public static event EventHandler<NotifyRequestEventArgs> NotifyRecieved;
+        public static event EventHandler<StartTransmissionEventArgs> StartTransmission;
+        public static event EventHandler<FinishEventArgs> Finish;
+        public static event EventHandler<StartTransmissionEventArgs> StartTransmissionAndRespSent;
     }
 }
