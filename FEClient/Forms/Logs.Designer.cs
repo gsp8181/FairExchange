a970eaf1b0a948486b2cbfc9c37bef9e63797635
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-04-14 12:23:47 +0100

    Started to complete the logging form

--- FEClient/Forms/Logs.Designer.cs
+++ FEClient/Forms/Logs.Designer.cs
@@ -28,11 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.sentTabPage = new System.Windows.Forms.TabPage();
+            this.receivedTabPage = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.sentTabPage);
+            this.tabControl.Controls.Add(this.receivedTabPage);
+            this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl.Location = new System.Drawing.Point(0, 0);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(284, 262);
+            this.tabControl.TabIndex = 0;
+            // 
+            // sentTabPage
+            // 
+            this.sentTabPage.Location = new System.Drawing.Point(4, 22);
+            this.sentTabPage.Name = "sentTabPage";
+            this.sentTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.sentTabPage.Size = new System.Drawing.Size(276, 236);
+            this.sentTabPage.TabIndex = 1;
+            this.sentTabPage.Text = "Sent";
+            this.sentTabPage.UseVisualStyleBackColor = true;
+            // 
+            // receivedTabPage
+            // 
+            this.receivedTabPage.Location = new System.Drawing.Point(4, 22);
+            this.receivedTabPage.Name = "receivedTabPage";
+            this.receivedTabPage.Size = new System.Drawing.Size(276, 236);
+            this.receivedTabPage.TabIndex = 2;
+            this.receivedTabPage.Text = "Received";
+            this.receivedTabPage.UseVisualStyleBackColor = true;
+            // 
+            // Logs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.tabControl);
+            this.Icon = global::FEClient.Properties.Resources.Icojam_Blue_Bits_Document_arrow_down;
+            this.Name = "Logs";
             this.Text = "Logs";
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage sentTabPage;
+        private System.Windows.Forms.TabPage receivedTabPage;
     }
 }
