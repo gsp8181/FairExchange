c176f692cc1a1428a33e8d9ebd6a86215e80983b
Geoffrey Prytherch <gsp8181@users.noreply.github.com>
2015-04-17 12:48:46 +0100

    Fixed bug on Notify

--- FEClient/Forms/SendDialog.cs
+++ FEClient/Forms/SendDialog.cs
@@ -76,7 +76,7 @@ namespace FEClient.Forms
 
         private void MyResource_StartTransmission(object sender, StartTransmissionEventArgs args)
         {
-            if (args.FileName != _file.Name || args.Guid != _guid || args.IP != _ip)
+            if (args.FileName != _file.Name || args.Guid != _guid/* || args.IP != _ip*/)
                 return;
             args.HasSet = true;
             Invoke((MethodInvoker) delegate { timeoutTimer.Stop(); });
